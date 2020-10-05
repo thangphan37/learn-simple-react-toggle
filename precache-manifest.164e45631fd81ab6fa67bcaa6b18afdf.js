d8c79988368e4e3cb9f9f6b629fab4ebbeb98f9b
thangphan37 <pcthangk59@gmail.com>
2020-10-06 06:52:55 +0900

    Updates

---
+++ precache-manifest.164e45631fd81ab6fa67bcaa6b18afdf.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "16a09d3acade6bb574e7526d31195f2a",
+    "url": "/learn-simple-react-toggle/index.html"
+  },
+  {
+    "revision": "dafa2d588e5260c31a84",
+    "url": "/learn-simple-react-toggle/static/css/main.c3fd3b46.chunk.css"
+  },
+  {
+    "revision": "b0024b71f7a7e544add1",
+    "url": "/learn-simple-react-toggle/static/js/2.7dd6299c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/learn-simple-react-toggle/static/js/2.7dd6299c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dafa2d588e5260c31a84",
+    "url": "/learn-simple-react-toggle/static/js/main.2a88f5b9.chunk.js"
+  },
+  {
+    "revision": "fc8329f36139c540a736",
+    "url": "/learn-simple-react-toggle/static/js/runtime-main.65764d75.js"
+  }
+]);
